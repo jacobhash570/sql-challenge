c0cf0845f498c7e0381d0c9c83a744e1d088e1e0
Jacob <jacob.hash570@gmail.com>
2021-08-17 19:50:01 -0400

    Employee hire.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,5 +1,12 @@
 --employee details
-select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-from employees as e
-left join salaries as s
-on e.emp_no = s.emp_no;
+-- select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+-- from employees as e
+-- left join salaries as s
+-- on e.emp_no = s.emp_no;
+
+--employee hire
+select * from employees;
+
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1986/1/1' and '1986/12/31';
