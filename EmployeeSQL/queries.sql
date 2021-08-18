9aa9d20e874b851d9aa4284acf797b2109f8758b
Jacob <jacob.hash570@gmail.com>
2021-08-17 20:28:36 -0400

    Hercules.

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,17 +1,17 @@
---employee details
+--employee details(1)
 -- select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 -- from employees as e
 -- left join salaries as s
 -- on e.emp_no = s.emp_no;
 
---employee hire
+--employee hire(2)
 -- select * from employees;
 
 -- select first_name, last_name, hire_date
 -- from employees
 -- where hire_date between '1986/1/1' and '1986/12/31';
 
--- department managers
+-- department managers(3)
 -- select d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
 -- from departments as d
 -- left join manager as m
@@ -19,11 +19,16 @@
 -- left join employees as e
 -- on m.emp_no = e.emp_no;
 
---departments
-select e.emp_no, e.last_name, e.first_name, d.dept_name
-from dept_employees as de
-left join departments as d
-on de.dept_no = d.dept_no
-left join employees as e
-on de.emp_no = e.emp_no;
+--departments(4)
+-- select e.emp_no, e.last_name, e.first_name, d.dept_name
+-- from dept_employees as de
+-- left join departments as d
+-- on de.dept_no = d.dept_no
+-- left join employees as e
+-- on de.emp_no = e.emp_no;
 
+--hercules and b(5)
+select e.first_name, e.last_name, e.sex
+from employees as e
+where e.first_name = 'Hercules' 
+and e.last_name like 'B%';
