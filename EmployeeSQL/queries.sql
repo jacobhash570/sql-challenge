dac5a41f6ae3a7cbc20a35392e02738fe9208fe7
Jacob <jacob.hash570@gmail.com>
2021-08-17 20:13:33 -0400

    Departments

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -12,10 +12,18 @@
 -- where hire_date between '1986/1/1' and '1986/12/31';
 
 -- department managers
-select d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
-from departments as d
-left join manager as m
-on d.dept_no = m.dept_no
+-- select d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
+-- from departments as d
+-- left join manager as m
+-- on d.dept_no = m.dept_no
+-- left join employees as e
+-- on m.emp_no = e.emp_no;
+
+--departments
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from dept_employees as de
+left join departments as d
+on de.dept_no = d.dept_no
 left join employees as e
-on m.emp_no = e.emp_no;
+on de.emp_no = e.emp_no;
 
